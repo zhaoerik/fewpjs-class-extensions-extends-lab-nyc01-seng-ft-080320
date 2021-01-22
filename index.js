d42f7a60aa7096f1584b22aff023d6fc06e0355e
Erik Zhao <erikzhao98@gmail.com>
2021-01-22 18:30:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,3 @@ class Triangle extends Polygon {
         return ((this.array[0] + this.array[1] >= this.array[2]) && (this.array[0] + this.array[2] >= this.array[1]) && (this.array[1] + this.array[2] >= this.array[0])) 
     }
 }
-
-let triangleOne = new Triangle([5, 5, 5])
-console.log(triangleOne.isValid())
