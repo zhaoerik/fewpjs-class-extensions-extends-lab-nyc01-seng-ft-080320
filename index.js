0b3cc4e75b553a4371e394413daefa3a7f714662
Erik Zhao <erikzhao98@gmail.com>
2021-01-22 18:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ class Polygon {
         return this.array.reduce((acc, cur) => acc + cur)
     }
 }
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.array.length != 3) return false
+        if (array[0] + array[1] > array[2] && array[0] + array[2] > array[1] && array[1] + array[2] > array[0]) return true
+        else return false
+    }
+}
