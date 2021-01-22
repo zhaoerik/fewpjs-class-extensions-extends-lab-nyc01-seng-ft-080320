03a24101fb94c1a709edb25c39002da5f8b28d01
Erik Zhao <erikzhao98@gmail.com>
2021-01-22 18:12:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Polygon {
 
 class Triangle extends Polygon {
     get isValid() {
-        if (this.countSides != 3) return
+        if (this.countSides !== 3) return
         return ((array[0] + array[1] >= array[2]) && (array[0] + array[2] >= array[1]) && (array[1] + array[2] >= array[0])) 
     }
 }
